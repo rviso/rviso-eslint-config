7127535e50575f510f9cd54daba855a5bc023906
wanglingan <wanglingan@mysteel.com>
2024-05-18 20:17:43 +0800

    ✨ feat: 新增@rviso/eslint-config包，用于集成所有配置

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  root: true, // 作用于当前目录及其子目录
+  extends: ['@rviso/eslint-config-standard'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['@rviso/eslint-config-typescript'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+    {
+      files: ['*.vue'],
+      extends: ['@rviso/eslint-config-vue'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+}
